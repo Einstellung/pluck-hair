f42488e55e852c72af139edd6f9412b9b415f1f1
Xinyuan <einstellungsu@gmail.com>
2025-11-05 12:28:58 +0800

    Remove PetsDataModule from data loader exports in __init__.py

--- train/baseline/src/data_loader/__init__.py
+++ train/baseline/src/data_loader/__init__.py
@@ -2,10 +2,8 @@
 
 from .base_datamodule import BaseDataModule
 from .voc_dataloader import VOC2012DataModule
-from .pets_dataloader import PetsDataModule
 
 __all__ = [
     "BaseDataModule",
     "VOC2012DataModule",
-    "PetsDataModule",
 ]
